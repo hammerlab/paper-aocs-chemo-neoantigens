c15eba34a31b9e9f3e6d15bdced439cf07eda87a
Tim O'Donnell <timodonnell@gmail.com>
2016-05-26 13:55:35 -0400

    update

--- R/signature_deconstruction.R
+++ R/signature_deconstruction.R
@@ -1,27 +1,30 @@
 library(deconstructSigs)
 library(reshape)
 
-raw_my_signatures <- read.csv("~/sinai/git/ovarian-paired-analyses/data/derived/all_signatures_for_deconstructsigs.csv",
-                          check.names = F,
-                          row.names = 1)
+raw_my_signatures <- read.csv(
+  "~/sinai/git/paper-201604/data/all_signatures_for_deconstructsigs_with_chicken.csv",
+  check.names = F,
+  row.names = 1)
 my_signatures <- subset(raw_my_signatures, select=colnames(signatures.cosmic))
+rownames(my_signatures) <- make.names(rownames(my_signatures))
 
-raw_input <- read.csv("~/sinai/git/ovarian-paired-analyses/data/derived/mutation_contexts_counts_extended.csv", check.names = F)
+raw_input <- read.csv("~/sinai/git/paper-201604/data/mutation_contexts_counts.csv", check.names = F)
 rownames(raw_input) <- paste(raw_input$source_id, raw_input$kind, sep=" ")
-input <- subset(raw_input, select=colnames(randomly.generated.tumors))
+raw_input$kind <- NULL
+raw_input$source_id <- NULL
 
 result <- list()
 for(sample in rownames(input)) {
   print(sample)
-  output <- whichSignatures(input,
-                            sample,
+  output <- whichSignatures(tumor.ref = raw_input,
+                            sample.id = sample,
                             contexts.needed = TRUE,
-                            tri.counts.method = "genome",
+                            tri.counts.method = "default",
                             signatures.ref = my_signatures)
-  makePie(output, sub=sample)
+  # makePie(output, sub=sample)
   output$weights['Sample'] <- sample
   result[[sample]] <- output$weights
 }
 
 final = reshape::merge_all(result)
-write.csv(final, '~/sinai/git/ovarian-paired-analyses/data/derived/deconstructsigs_output_extended.csv')
+write.csv(final, '~/sinai/git/paper-201604/data/deconstructsigs_output_with_chicken.csv')
